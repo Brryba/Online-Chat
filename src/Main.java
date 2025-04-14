25d2f0a5416fbeb40137a439eafeaea99f253db4
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-04-14 11:19:15 +0300

    TCP messages communication done

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,28 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Main {
     private static final int CONNECTION_PORT_UDP = 50_000;
     private static final int CONNECTION_PORT_TCP = 50_001;
-    private static final String LOCAL_ADDRESS = "127.0.0.52";
+    private static final String LOCAL_ADDRESS = "127.0.0.3";
     private static final String BROADCAST_ADDRESS = "255.255.255.255";
-    private static final ConcurrentHashMap<String, Socket> sockets = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<Socket, String> connectedUsersNames = new ConcurrentHashMap<>();
+
+    private static String CURRENT_USER_NAME = "";
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in);
              ServerSocket serverSocket = new ServerSocket(CONNECTION_PORT_TCP, 50, InetAddress.getByName(LOCAL_ADDRESS))) {
 
             System.out.println("Enter your name:");
-            String userName = scanner.nextLine();
+            CURRENT_USER_NAME = scanner.nextLine();
 
-            sendBroadcastUDP(userName);
+            sendBroadcastUDP(CURRENT_USER_NAME);
             new Thread(() -> receiveAllTCP(serverSocket)).start();
             new Thread(Main::receiveUDPRequest).start();
 
             while (true) {
-                Thread.sleep(3000);
-                System.out.println(sockets);
+                byte[] input = scanner.nextLine().getBytes();
+                for (Socket socket : connectedUsersNames.keySet()) {
+                    socket.getOutputStream().write(input);
+                }
             }
         } catch (Exception e) {
             System.err.println("Error: " + e);
@@ -61,27 +65,40 @@ public class Main {
 
     private static void startTCPConnection(String address, String userName) throws Exception {
         Socket socket = new Socket(address, CONNECTION_PORT_TCP, InetAddress.getByName(LOCAL_ADDRESS), 0);
-        sockets.put(userName, socket);
-        while (!socket.isClosed()) {
-            Thread.sleep(1000);
-        }
+        connectedUsersNames.put(socket, userName);
+        socket.getOutputStream().write(CURRENT_USER_NAME.getBytes());
+        receiveTCPMessages(socket);
     }
 
     private static void receiveAllTCP(ServerSocket serverSocket) {
         try {
             while (true) {
                 Socket newSocket = serverSocket.accept();
-                System.out.println("New TCP connection: " + newSocket);
                 new Thread(() -> {
                     try {
-                        sockets.put(newSocket.getInetAddress().getHostAddress(), newSocket);
-                        while (!newSocket.isClosed()) {
-                            Thread.sleep(1000);
-                        }
+                        byte[] bytes = new byte[1024];
+                        int bytesRead = newSocket.getInputStream().read(bytes);
+                        String name = new String(bytes, 0, bytesRead);
+                        connectedUsersNames.put(newSocket, name);
+                        receiveTCPMessages(newSocket);
                     } catch (Exception e) {
                         System.err.println("Error handling TCP connection: " + e);
                     }
                 }).start();
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e);
+        }
+    }
+
+    private static void receiveTCPMessages(Socket socket) {
+        try {
+            while (!socket.isClosed()) {
+                byte[] bytes = new byte[1024];
+                int bytesRead = socket.getInputStream().read(bytes);
+                String input = new String(bytes, 0, bytesRead);
+                System.out.println(socket.getInetAddress().getHostAddress() + " " + connectedUsersNames.get(socket)
+                        + " : " + input);
             }
         } catch (Exception e) {
             System.err.println("Error: " + e);
